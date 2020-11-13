9fe98b9954bac372f18995529fc406c5033db1ec
shandean <deanshanm@163.com>
2020-11-13 16:15:25 +0800

    update

--- src/data.js
+++ src/data.js
@@ -8,16 +8,16 @@ export const MatchDataType = (target, type) => Object.is(_toString.call(target).
 
 // filter data type for example null/undefined/""/[]/{}
 export const FilterDataType = params =>
-    matchDataType(params, "Null") ||
-    matchDataType(params, "Undefined") ||
-    (matchDataType(params, "String") && params.trim() === "") ||
-    (matchDataType(params, "Array") && params.length === 0) ||
-    (matchDataType(params, "Object") && Object.keys(params).length === 0);
+    MatchDataType(params, "Null") ||
+    MatchDataType(params, "Undefined") ||
+    (MatchDataType(params, "String") && params.trim() === "") ||
+    (MatchDataType(params, "Array") && params.length === 0) ||
+    (MatchDataType(params, "Object") && Object.keys(params).length === 0);
 
 // filter objects
 export const FilterObject = object => {
     let obj = {};
-    Object.keys(object).map(key => !filterDataType(object[key]) && (obj[key] = object[key]));
+    Object.keys(object).map(key => !FilterDataType(object[key]) && (obj[key] = object[key]));
     return obj;
 }
 
@@ -29,11 +29,11 @@ export const FilterObject = object => {
  * @param {string} value 默认返回值
  */
 export const GetTargetVal = ({target, key, value = ""}) => {
-    return filterDataType(target)
+    return FilterDataType(target)
         ? value
         : Object.is(key, undefined)
             ? target
-            : filterDataType(target[key])
+            : FilterDataType(target[key])
                 ? value
                 : target[key];
 }
@@ -45,9 +45,9 @@ export const GetTargetVal = ({target, key, value = ""}) => {
  * @param {string} key 目标常量的key
  */
 export const GetTargetConst = (constant, value, key) => {
-    if (filterDataType(value)) return "--";
+    if (FilterDataType(value)) return "--";
 
-    if (getDataType(value, "String")) {
+    if (GetDataType(value, "String")) {
         let arr = constant.filter(v => v.value === value);
         return arr.length === 0 ? "--" : arr[0][key];
     }
